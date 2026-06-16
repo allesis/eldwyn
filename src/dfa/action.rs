94c9c9c648aa26284a6403da47e433c12b2d2a23
Sean Allesina-McGrory <allesis@bu.edu>
2026-06-15 20:51:02 -0700

    Simplify DFA struct
    Previously added a bunch of noise prematurely. Will likely readd later
    but the clutter was stressing me so dropping till its actually needed.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@bu.edu>

--- src/dfa/action.rs
+++ src/dfa/action.rs
@@ -1,10 +1,4 @@
 pub(crate) enum Action {
-    MatchSymbol { symbol: char },
-    MatchDigit,
-    MatchLetter,
-    MatchWhitespace,
-    MatchStartOfString,
-    MatchEndOfString,
-    MatchSet,
-    CaptureGroup,
+    Accept,
+    MatchSymbol { symbol: u8 },
 }
--- src/dfa/mod.rs
+++ src/dfa/mod.rs
@@ -31,31 +31,4 @@ impl DFA {
     pub(crate) fn run(&self, input: &str) -> bool {
         todo!()
     }
-
-    fn validate_pattern(&self, pattern: &mut str) -> Result<(), Error> {
-        fn capture_group(pattern: &mut str) -> Result<(), Error> {
-            todo!()
-        }
-
-        fn match_group(pattern: &mut str) -> Result<(), Error> {
-            todo!()
-        }
-
-        fn digit(pattern: &mut str) -> Result<(), Error> {
-            todo!()
-        }
-
-        fn letter(pattern: &mut str) -> Result<(), Error> {
-            todo!()
-        }
-
-        fn whitespace(pattern: &mut str) -> Result<(), Error> {
-            todo!()
-        }
-        loop {
-            if let Some((current, pattern)) = pattern.split_at_mut_checked(1) {
-                todo!()
-            }
-        }
-    }
 }
