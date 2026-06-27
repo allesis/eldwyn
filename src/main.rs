c39e7044434b0d7fb28f6002c67ded0b5b134c40
Sean Allesina-McGrory <allesis@bu.edu>
2026-06-26 18:13:06 -0700

    Add simple tests for basic regex
    Currently, can match symbols but no metacharacters. So test only simple
    patterns.
    
    Signed-off-by: Sean Allesina-McGrory <allesis@bu.edu>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use eldwyn::Regex;
+#[test]
+fn doesnt_halt_and_catch_fire() {
+    use eldwyn::Regex;
 
-fn main() {
-    let mut pattern = Vec::from("Hello, Cruel World!");
-    let _ = Regex::new(&mut pattern);
+    let re = Regex::new("").expect("Failed to construct regex from pattern");
+    assert!(re.run(""));
 }
+
+#[test]
+fn can_match_simple_patterns() {
+    use eldwyn::Regex;
+
+    let re = Regex::new("hello").expect("Failed to construct regex from pattern");
+    assert!(re.run("hello"));
+}
+
+fn main() {}
